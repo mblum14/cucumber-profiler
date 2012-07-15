b1e0f15b9f7c0643f8b0e113331a1bef01e3ebf8
Michael Blumberg <mblum14@gmail.com>
2012-07-15 09:24:34 -0400

    Potential fix for Scenario method missing

--- lib/cucumber-profiler.rb
+++ lib/cucumber-profiler.rb
@@ -1,10 +1,9 @@
 require 'cucumber/formatter/profiler'
-
-# Extend Cucumber's builtin formats, so that this
-# formatter can be used with --format fuubar
+require 'cucumber/ast/benchmark'
 require 'cucumber/cli/options'
 
-Cucumber::Cli::Options::BUILTIN_FORMATS["Profiler"] = [
+Scenario.extend Cucumber::Ast::Benchmark
+Cucumber::Cli::Options::BUILTIN_FORMATS["profiler"] = [
   "Cucumber::Formatter::Profiler",
   "A formatter for benchmarking your features"
 ]
