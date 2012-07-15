53d7c3f39b5041e44484c4a501faab1f8fecbe3f
Michael Blumberg <mblum14@gmail.com>
2012-07-15 09:52:03 -0400

    Fix for adding profiler to the builtin formatters

--- cucumber-profiler.gemspec
+++ cucumber-profiler.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = "cucumber-profiler"
-  s.version     = "0.0.17"
+  s.version     = "0.0.18"
   s.platform    = Gem::Platform::RUBY
   s.authors     = ["Michael Blumberg"]
   s.email       = ["mblum14@gmail.com"]
--- lib/cucumber-profiler.rb
+++ lib/cucumber-profiler.rb
@@ -2,7 +2,7 @@ require 'cucumber/formatter/profiler'
 require 'cucumber/ast/benchmark'
 require 'cucumber/cli/options'
 
-Cucumber::Cli::Options::BUILTIN_FORMATS["profiler"] = [
+Cucumber::Cli::Options::BUILTIN_FORMATS["profiler"] => [
   "Cucumber::Formatter::Profiler",
   "A formatter for benchmarking your features"
 ]
